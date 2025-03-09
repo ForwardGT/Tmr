8a6e8732f82a030be2d346859d1ef8c0702e0ddc
ForwardGT <ollmaincore@gmail.com>
2025-03-10 01:00:36 +0300

    fix transparent windows + jdk 17

--- composeApp/src/desktopMain/kotlin/org/example/project/App.kt
+++ composeApp/src/desktopMain/kotlin/org/example/project/App.kt
@@ -1,20 +1,24 @@
 package org.example.project
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import io.ktor.client.request.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import org.example.project.api.data.network.KtorClient
+import androidx.compose.ui.unit.sp
+import org.example.project.api.RemoteImage
+import org.example.project.core.store.TmrStore
 import org.example.project.core.ui.components.ExitButton
 import org.example.project.core.ui.components.SwitchTmr
-import org.example.project.core.utils.constant.Constants.VERSION
+import org.example.project.core.ui.resourses.TmrColors
+import org.example.project.core.utils.constant.Constants.BASE_URL_IMAGE
 import org.example.project.main_window.shutdown_timer.ShutdownTimer
 import org.example.project.main_window.work_timer.WorkTimer
+import org.koin.compose.viewmodel.koinViewModel
+
 
 @Composable
 fun App(closeApp: () -> Unit) {
@@ -22,34 +26,48 @@ fun App(closeApp: () -> Unit) {
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center,
     ) {
-        var state by remember { mutableStateOf(false) }
+        val store: TmrStore = koinViewModel()
+        val state by store.state.collectAsState()
+
+        Column(
+            modifier = Modifier
+            .fillMaxWidth()
+            .align(Alignment.TopCenter)
+        ) {
+            Spacer(modifier = Modifier.padding(top = 110.dp))
+            val url = "$BASE_URL_IMAGE${state.weather.iconCode}@2x.png"
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+            ) {
+                Text(
+                    modifier = Modifier.padding(top = 22.dp),
+                    text = "${state.weather.temperature}\u00B0C",
+                    fontSize = 20.sp,
+                    color = TmrColors.mainText,
+                )
+
+                RemoteImage(url = url, modifier = Modifier.size(60.dp))
+
+                Text(
+                    modifier = Modifier.padding(top = 22.dp),
+                    text = "${state.weather.windSpeed}m/s",
+                    fontSize = 20.sp,
+                    color = TmrColors.mainText,
+                )
+            }
+        }
 
         Row(
             modifier = Modifier.fillMaxWidth().align(Alignment.TopCenter),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            SwitchTmr(state = state) {
-                state = it
-            }
+            SwitchTmr(state = state.isTimer) { store.switchTimer(it) }
             ExitButton(closeApp)
         }
-
-        if (state) {
-            WorkTimer(modifier = Modifier.size(250.dp))
-        } else {
-
-            val scope = CoroutineScope(Dispatchers.IO)
-            val client = KtorClient.client
-            scope.launch {
-                client.get("$VERSION/forecast?latitude=55.75&longitude=37.61&current_weather=true")
-                client.close()
-            }
-
-
-            ShutdownTimer(
-                modifier = Modifier.size(250.dp),
-                totalTime = 3600L * 1000L,
-            )
+        Box {
+            if (state.isTimer) WorkTimer(modifier = Modifier.size(250.dp))
+            else ShutdownTimer(modifier = Modifier.size(250.dp), totalTime = 3600L * 1000L)
         }
     }
 }
